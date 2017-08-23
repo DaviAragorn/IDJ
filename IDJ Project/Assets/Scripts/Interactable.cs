633e4132c2caafb94e46b6dde1b0df07f6ec4d8e
Takimiro <Takimiro@hotmail.com>
2017-08-23 12:23:39 -0300

    Character and Movement

---
+++ IDJ Project/Assets/Scripts/Interactable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 1f;
+    // Use this for initialization
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
