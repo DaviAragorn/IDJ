ec29a5325dcb56333f9aabee00e8cb241db71853
Takimiro <Takimiro@hotmail.com>
2017-09-02 11:49:50 -0300

    House sript

---
+++ IDJ Project/Assets/Scripts/Enterable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enterable : Interactable {
+    public string LevelName;
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevel(LevelName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
